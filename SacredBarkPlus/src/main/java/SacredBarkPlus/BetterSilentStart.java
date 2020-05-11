b6635a797b96ea0be427d4217be5ac5939769008
erasels <emanuelbedburdick@yahoo.de>
2020-05-11 22:27:32 +0200

    initial version

--- SacredBarkPlus/src/main/java/SacredBarkPlus/BetterSilentStart.java
+++ SacredBarkPlus/src/main/java/SacredBarkPlus/BetterSilentStart.java
@@ -8,7 +8,19 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 @SpireInitializer
 public class BetterSilentStart implements
-        PostInitializeSubscriber {
+        PostInitializeSubscriber,
+        AddAudioSubscriber{
+
+    public static final String ELITE = "NodeSpeaker:elite";
+    public static final String ENEMY = "NodeSpeaker:enemy";
+    public static final String MERCHANT = "NodeSpeaker:merchant";
+    public static final String REST = "NodeSpeaker:rest";
+    public static final String TREASURE = "NodeSpeaker:treasure";
+    public static final String UNKNOWN = "NodeSpeaker:unknown";
+
+
+
+
     public static void initialize() {
         BaseMod.subscribe(new BetterSilentStart());
     }
@@ -18,4 +30,14 @@ public class BetterSilentStart implements
 
     }
 
+
+    @Override
+    public void receiveAddAudio() {
+        BaseMod.addAudio(ELITE, "nodeSpeakerResources/audio/elite.mp3");
+        BaseMod.addAudio(ENEMY, "nodeSpeakerResources/audio/enemy.mp3");
+        BaseMod.addAudio(MERCHANT, "nodeSpeakerResources/audio/merchant.mp3");
+        BaseMod.addAudio(REST, "nodeSpeakerResources/audio/rest.mp3");
+        BaseMod.addAudio(TREASURE, "nodeSpeakerResources/audio/treasure.mp3");
+        BaseMod.addAudio(UNKNOWN, "nodeSpeakerResources/audio/unknown.mp3");
+    }
 }
---
+++ SacredBarkPlus/src/main/java/SacredBarkPlus/patches/AntiAudioBufferCrashPatch.java
@@ -0,0 +1,24 @@
+package SacredBarkPlus.patches;
+
+import basemod.BaseMod;
+import com.badlogic.gdx.backends.lwjgl.audio.OpenALMusic;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import javassist.CtBehavior;
+
+@SpirePatch(clz = OpenALMusic.class, method = "play")
+public class AntiAudioBufferCrashPatch {
+    @SpireInsertPatch(locator = Locator.class, localvars = {"errorCode"})
+    public static SpireReturn patch(OpenALMusic __instance, int errorCode) {
+        BaseMod.logger.info("Intercepted audio buffer crash, Error Code: " + errorCode);
+        return SpireReturn.Return(null);
+    }
+
+    private static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctBehavior) throws Exception {
+            Matcher finalMatcher = new Matcher.NewExprMatcher(GdxRuntimeException.class);
+            return LineFinder.findInOrder(ctBehavior, finalMatcher);
+        }
+    }
+}
