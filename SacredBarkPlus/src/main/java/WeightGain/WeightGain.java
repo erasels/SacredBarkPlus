b4cb681805ae2ef34cd91a8b971b2618582aa7bd
erasels <emanuelbedburdick@yahoo.de>
2021-05-24 16:29:18 +0200

    Create

--- SacredBarkPlus/src/main/java/WeightGain/WeightGain.java
+++ SacredBarkPlus/src/main/java/WeightGain/WeightGain.java
@@ -1,8 +1,6 @@
 package WeightGain;
 
-import WeightGain.patches.AllPatches;
 import basemod.BaseMod;
-import basemod.abstracts.CustomSavable;
 import basemod.interfaces.OnStartBattleSubscriber;
 import basemod.interfaces.PostInitializeSubscriber;
 import basemod.interfaces.PreStartGameSubscriber;
@@ -24,27 +22,14 @@ public class WeightGain implements
 
     @Override
     public void receivePostInitialize() {
-        BaseMod.addSaveField("weightGain:Feeds", new CustomSavable<Integer>() {
-            @Override
-            public Integer onSave() {
-                return AllPatches.feeds;
-            }
-
-            @Override
-            public void onLoad(Integer i) {
-                AllPatches.feeds = i;
-            }
-        });
     }
 
     @Override
     public void receivePreStartGame() {
-        AllPatches.feeds = 0;
-        AllPatches.fasts = 0;
     }
 
     @Override
     public void receiveOnBattleStart(AbstractRoom abstractRoom) {
-        AllPatches.fasts = 0;
+        
     }
 }
---
+++ SacredBarkPlus/src/main/java/WeightGain/patches/bad/ThreadCompleteListener.java
@@ -0,0 +1,5 @@
+package WeightGain.patches.bad;
+
+public interface ThreadCompleteListener {
+    void notifyOfThreadComplete(final Thread thread, String s, int counter);
+}
